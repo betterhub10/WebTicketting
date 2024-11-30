7880d0ab769f5b025c171b7db290deda25cd0678
USER <USER@USER>
2024-12-01 04:48:04 +0800

    Add project files.

---
+++ WebTicketting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebTicketting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
